93fcd9e6497b6b9a88365dbce1e337278e943fac
A.Nasiri <50527356+A-Nasiri@users.noreply.github.com>
2019-12-28 00:21:17 +0000

    Updating Mappable interface to receive info window content

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -4,7 +4,8 @@ interface Mappable {
   location: {
     lat: number;
     lng: number;
-  }
+  };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -21,12 +22,19 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
-    })
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
   }
 }
